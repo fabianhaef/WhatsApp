1357004a1b2bcd1ee0941c9aa8994f803a2a0c30
fabianhaef <fabian.haefi@gmail.com>
2023-08-31 23:09:47 +0200

    add shadow to message components

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -23,12 +23,20 @@ const Message = ({ message }) => {
 
 const styles = StyleSheet.create({
     container: {
-        // backgroundColor: 'white',
-        // alignSelf: 'flex-start',
         margin: 5,
         padding: 10,
         borderRadius: 10,
         maxWidth: '80%',
+
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 1,
     },
     time: {
         color: 'gray',
